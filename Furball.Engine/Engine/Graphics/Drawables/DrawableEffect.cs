154174c818d7113c6a77745d94aaf0bc578ee1f5
Eevee <developerbrzoza@gmail.com>
2021-09-24 16:03:16 +0200

    Post-Processing: initial implementation of EffectDrawableManager

---
+++ Furball.Engine/Engine/Graphics/Drawables/DrawableEffect.cs
@@ -0,0 +1,7 @@
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Furball.Engine.Engine.Graphics.Drawables {
+    public abstract class DrawableEffect {
+        public abstract Texture2D Draw(Texture2D input);
+    }
+}
---
+++ Furball.Engine/Engine/Graphics/Drawables/Managers/EffectDrawableManager.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Furball.Engine.Engine.Helpers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Furball.Engine.Engine.Graphics.Drawables.Managers {
+    public class EffectDrawableManager : UnmanagedDrawable {
+        private DrawableManager      _drawableManager = new();
+        private List<DrawableEffect> _effects         = new();
+        private RenderTarget2D       _target2D;
+
+        private GraphicsDevice _graphicsDevice;
+
+        public EffectDrawableManager(GraphicsDevice graphicsDevice) {
+            this._graphicsDevice = graphicsDevice;
+        }
+
+        public override void Draw(GameTime time, SpriteBatch batch, DrawableManagerArgs args = null) {
+            Texture2D currentPass = this._drawableManager.DrawRenderTarget2D(time, batch, args);
+
+            if(this._target2D?.Width != FurballGame.WindowWidth || this._target2D?.Height != FurballGame.WindowHeight)
+                this._target2D = new RenderTarget2D(this._graphicsDevice, FurballGame.WindowWidth, FurballGame.WindowHeight);
+
+            this._graphicsDevice.SetRenderTarget(this._target2D);
+            this._graphicsDevice.Clear(Color.Transparent);
+
+            for (int i = 0; i != this._effects.Count; i++) {
+                DrawableEffect currentEffect = this._effects[i];
+
+                currentPass = currentEffect.Draw(currentPass);
+
+                batch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
+                batch.Draw(currentPass, Vector2.Zero, Color.White);
+                batch.End();
+            }
+
+            this._graphicsDevice.SetRenderTarget(null);
+
+            batch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
+            batch.Draw(currentPass, Vector2.Zero, Color.White);
+            batch.End();
+        }
+
+        public void Add(BaseDrawable drawable) => this._drawableManager.Add(drawable);
+    }
+}
